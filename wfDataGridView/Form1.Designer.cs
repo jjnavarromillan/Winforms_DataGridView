990d380f7109095991ac9db595c8f45ba57850f6
jjnavarromillan@gmail.com <jjnavarromillan@gmail.com>
2019-05-26 14:51:46 -0700

    Second Commit

---
+++ wfDataGridView/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace wfDataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(836, 503);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellEndEdit);
+            this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DataGridView1_MouseDown);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(221, 8);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(203, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(3, 8);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(203, 23);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "Clear";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.ButtonRefresh_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(752, 8);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 552);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
---
+++ wfDataGridView/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfDataGridView
+{
+    public partial class Form1 : Form
+    {
+        ContextMenu contextMenu;
+        List<Customer> customers;
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        public void init() {
+            contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add("Delete");
+            contextMenu.MenuItems[0].Click += new EventHandler(contextMenu_Delete_ItemClicked);
+            contextMenu.MenuItems.Add("Add");
+            contextMenu.MenuItems[1].Click += new EventHandler(contextMenu_Add_ItemClicked);
+            dataGridView1.ContextMenu = contextMenu;
+            customers = SeedCustomers(); ;
+            Reload();
+        }
+
+        public void Reload() {
+            
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = customers;
+            dataGridView1.Refresh();
+        }
+
+        public void ClearIsntSaved() {
+            foreach (Customer customer in customers.ToList()) {
+                if (customer.ID == -1) {
+                    customers.Remove(customer);
+                }
+            }
+            Reload();
+        }
+        public void SaveElements() {
+            int max = 1;
+            if (customers.Where(x => x.ID > 0).Count() > 0)
+            {
+                max = customers.Max(x => x.ID) + 1;
+            }
+            foreach (Customer customer in customers.ToList())
+            {
+                if (customer.ID == -1)
+                {
+                    customer.ID = max;
+                    max++;
+                }
+            }
+            //Save in DB
+            Reload();
+        }
+
+
+        private void DataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Right:
+                    {
+                        contextMenu.Show(this, new Point(e.X, e.Y));
+
+                    }
+                    break;
+            }
+        }
+
+        private void contextMenu_Delete_ItemClicked(object sender, EventArgs e) {
+            DataGridViewSelectedRowCollection rows = dataGridView1.SelectedRows;
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows) {
+                deleteCustomer((int) row.Cells[0].Value);
+            }
+            Reload();
+        }
+
+        private void deleteCustomer(int ID) {
+             Customer customer= customers.Where(x=>x.ID == ID).FirstOrDefault();
+            if (customer != null) {
+                customers.Remove(customer);
+            }
+        }
+
+        private void contextMenu_Add_ItemClicked(object sender, EventArgs e)
+        {
+
+            AddCustomer();
+            Reload();
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].ReadOnly = false;
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].ReadOnly = true;
+        }
+
+        public List<Customer> SeedCustomers() {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer() { ID = 1, Name = "Joe", City = "Camarillo" },
+                new Customer() { ID = 2, Name = "Pitter", City = "Oxnard" },
+                new Customer() { ID = 3, Name = "Marie", City = "Ventura" },
+                new Customer() { ID = 4, Name = "Alfred", City = "Santa Barbara" },
+                new Customer() { ID = 5, Name = "Smith", City = "Camarillo" },
+                new Customer() { ID = 6, Name = "Marlon", City = "Ojai" },
+                new Customer() { ID = 7, Name = "Joseph", City = "Filrmore" }
+            };
+            return customers;
+
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            AddCustomer();
+            Reload();
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].ReadOnly = false;
+            dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].ReadOnly = true;
+        }
+
+        private void AddCustomer() {
+            customers.Add(new Customer() { ID = -1, Name = "Click Name", City = "Change City" });
+            //Save in DB
+        }
+
+
+        private void ButtonRefresh_Click(object sender, EventArgs e)
+        {
+            ClearIsntSaved();
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            SaveElements();
+        }
+       
+
+        private void UpdateCustomer(int row, int col)
+        {
+            //Save in DB
+         
+        }
+
+        private void DataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            int col = e.ColumnIndex;
+            string ColumnName = dataGridView1.Columns[col].Name;
+            if (col > 0)
+            {
+                UpdateCustomer(row,col);
+            }
+        }
+    }
+}
